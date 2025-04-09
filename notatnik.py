0df9498ffb29fd2e5d4247957272772ffdbc6f2e
magdalenamal <magmalska@gmail.com>
2025-04-09 14:52:03 +0200

    wip: added remove user function

---
+++ notatnik.py
@@ -0,0 +1,8 @@
+users: list = [
+    {'name': 'Julia', 'location': 'Ząbki', 'posts': 10},
+    {'name': 'Klaudia', 'location': 'Warszawa', 'posts': 10},
+    {'name': 'Szymon', 'location': 'Ostrołęka', 'posts': 10},
+]
+
+print(users)
+
