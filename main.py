8035ee9d3a59b04919fd4469ef6e735c106549c7
magdalenamal <magmalska@gmail.com>
2025-04-14 12:24:20 +0200

    wip: added update user function

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from utils.model import users
-from utils.controller import get_user_info, add_user, remove_user
+from utils.controller import get_user_info, add_user, remove_user, update_user
 
 
 def main():
+        print(f'Witaj {users[0]["name"]}')
         while True:
                 print("=====MENU=====")
                 print("0 - Zakończ program")
                 print("1 - Wyświetl znajomych")
                 print("2 - Dodaj znajomego")
                 print("3 - Usuń znajomego")
+                print("4 - Zaktualizuj dane o znajomym")
                 print("==============")
 
                 choice = input("Wybierz opcję Menu")
@@ -16,6 +18,7 @@ def main():
                 if choice == "1":get_user_info(users[1:])
                 if choice == "2":add_user(users)
                 if choice == "3":remove_user(users)
+                if choice == "4":update_user(users[1:])
 
 
 if __name__ == '__main__':
