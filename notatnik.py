475e249a9f01ddcd2ed74a63e1528ab6517a5279
magdalenamal <magmalska@gmail.com>
2025-06-02 08:20:23 +0200

    wip: fronted z interaktywną mapą

--- notatnik.py
+++
@@ -1,8 +0,0 @@
-users: list = [
-    {'name': 'Julia', 'location': 'Ząbki', 'posts': 10},
-    {'name': 'Klaudia', 'location': 'Warszawa', 'posts': 10},
-    {'name': 'Szymon', 'location': 'Ostrołęka', 'posts': 10},
-]
-
-print(users)
-
